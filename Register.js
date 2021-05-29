50a86c9a242dd858ffb98be531c2cd4e0e068d64
Akhzar Nazir <akhzarn@yahoo.com>
2021-05-29 12:54:18 +0500

    NCBAE First Commit

---
+++ Register.js
@@ -0,0 +1,123 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+ import React from 'react';
+ import type {Node} from 'react';
+ import {
+   SafeAreaView,
+   ScrollView,
+   StatusBar,
+   StyleSheet,
+   Text,
+   useColorScheme,
+   View,
+   TextInput,
+   Button,
+   Alert
+ } from 'react-native';
+ 
+ import {
+   Colors,
+   DebugInstructions,
+   Header,
+   LearnMoreLinks,
+   ReloadInstructions,
+ } from 'react-native/Libraries/NewAppScreen';
+ 
+ const Section = ({children, title}): Node => {
+   const isDarkMode = useColorScheme() === 'dark';
+   
+   
+   return (
+ 
+ 
+     <View style={styles.sectionContainer}>
+       <Text
+         style={[
+           styles.sectionTitle,
+           {
+             color: isDarkMode ? Colors.white : Colors.black,
+           },
+         ]}>
+         {title}
+       </Text>
+       <Text
+         style={[
+           styles.sectionDescription,
+           {
+             color: isDarkMode ? Colors.light : Colors.dark,
+           },
+         ]}>
+         {children}
+       </Text>
+     </View>
+   );
+ };
+ 
+ const Register: () => Node = () => {
+   const isDarkMode = useColorScheme() === 'dark';
+ 
+   const backgroundStyle = {
+     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+   };
+ 
+   return (
+    
+     <View>
+       <Text> Ecommerce App </Text>
+ 
+       <TextInput style={styles.saboor}
+         placeholder="Enter Username"
+       />
+ 
+       <TextInput
+         placeholder="Enter password"
+         secureTextEntry={true}
+       />
+ 
+ 
+ <Button
+   title="Sign In"
+   color="#841584"
+   accessibilityLabel="Learn more about this purple button"
+   onPress={() => Alert.alert('NCBAE')}
+ />
+ 
+ 
+     </View>
+ 
+   );
+ };
+ 
+ const styles = StyleSheet.create({
+ 
+   saboor:{
+      backgroundColor:'red', 
+       alignContent:'center',
+       textAlign:'center',
+   },
+   sectionContainer: {
+     marginTop: 32,
+     paddingHorizontal: 24,
+   },
+   sectionTitle: {
+     fontSize: 24,
+     fontWeight: '600',
+   },
+   sectionDescription: {
+     marginTop: 8,
+     fontSize: 18,
+     fontWeight: '400',
+   },
+   highlight: {
+     fontWeight: '700',
+   },
+ });
+ 
+ export default Register;
+ 
